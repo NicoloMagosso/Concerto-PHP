d3b3029df741efb6fe69ab9e75df679abf01eec4
Name <98811739+NicoloMagosso@users.noreply.github.com>
2023-10-19 21:33:44 +0200

    Delete OrganizzazioneConcerto.sql

--- OrganizzazioneConcerto.sql
+++
@@ -1,8 +0,0 @@
-create database OrganizzazioneConcerto;
-create table organizzazioneconcerto.concerti(
-_id int not null auto_increment primary key,
-_codice varchar(50),
-_titolo varchar(50),
-_descrizione varchar(100),
-_data datetime
-);
